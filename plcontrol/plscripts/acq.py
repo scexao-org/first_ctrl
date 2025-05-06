930bc291dea0531de722ce96f5efb571a276ed57
sylacour <sylvestre.lacour@obspm.fr>
2025-05-06 19:23:33 +0200

    New plot to find optimal position for flux

--- plcontrol/plscripts/acq.py
+++ plcontrol/plscripts/acq.py
@@ -4,6 +4,8 @@ import time
 import numpy as np
 import os
 from astropy.io import fits
+from scipy.interpolate import griddata
+from scipy.optimize import curve_fit
 
 class Acquisition(Base):
     def __init__(self, *args, **kwargs):
@@ -79,3 +81,97 @@ class Acquisition(Base):
         self._ld.start_output_trigger()#//(delay = delay) # TODO - need to flash new code
         self._db.validate_last_tc()        
         return None
+    
+
+    def opti_flux(data_path = "/mnt/datazpool/PL/") :
+        """
+        After running opti_scan to run a grid scan, display the associated 
+        flux map to find the position maximizing the flux
+        
+        * maybe we want the SCAN data to be saved with a specific name (including "scan" in the filename for instance)
+        """
+
+        # Function to find the latest fits file that was writen
+        def find_most_recent_fits_file(directory):
+            most_recent_file = None
+            most_recent_mtime = 0
+
+            for root, _, files in os.walk(directory):
+                for filename in files:
+                    if filename.endswith('.fits'):
+                        filepath = os.path.join(root, filename)
+                        if os.path.isfile(filepath):
+                            mtime = os.path.getmtime(filepath)
+                            if mtime > most_recent_mtime:
+                                most_recent_mtime = mtime
+                                most_recent_file = filepath
+
+            return most_recent_file
+
+        # Define a 2D Gaussian function
+        def gaussian_2d(xy, amplitude, xo, yo, sigma, offset):
+            x, y = xy
+            xo = float(xo)
+            yo = float(yo)
+            w = 1/(sigma**2)
+            g = offset + amplitude * np.exp(-(w*((x-xo)**2) + w*((y-yo)**2)))
+            return g.ravel()
+        
+
+        # finding the most recent dataset:
+        most_recent = find_most_recent_fits_file(data_path)
+
+        if most_recent:
+            print(f"Most recent .fits file: {most_recent}")
+        else:
+            print("No .fits files found.")
+
+        # reading the modulation function
+        hdu = fits.open(most_recent)
+        xmod = hdu[1].data['xmod']
+        ymod = hdu[1].data['ymod']
+
+        # reading the flux
+        fluxes = np.mean(hdu[0].data, axis=(1,2))
+        xmin, xmax   = np.min(xmod), np.max(xmod)
+        ymin, ymax   = np.min(ymod), np.max(ymod)
+
+        # Define the grid for interpolation
+        grid_x, grid_y = np.mgrid[xmin:xmax:500j, ymin:ymax:500j]  # 500x500 grid
+
+        # Interpolate the fluxes onto the grid
+        flux_map = griddata((xmod, ymod), fluxes, (grid_x, grid_y), method='cubic')
+
+        # Prepare data for fitting
+        z = fluxes
+        x = xmod
+        y = ymod
+        amplitude_0=np.max(fluxes)-np.min(fluxes)
+        x_0= x[fluxes.argmax()]
+        y_0= y[fluxes.argmax()]
+        sigma_0 = (x.max()-x.min())/4
+        offset_0=np.min(fluxes)
+
+        # Initial guess for the parameters
+        initial_guess = (amplitude_0,x_0,y_0,sigma_0,offset_0)
+
+        # Fit the Gaussian
+        popt, _ = curve_fit(gaussian_2d, (x, y), z, p0=initial_guess)
+        x_fit=popt[1]
+        y_fit=popt[2]
+
+        # Generate the fitted Gaussian for plotting
+        fitted_gaussian = gaussian_2d((grid_x, grid_y), *popt).reshape(grid_x.shape)
+
+        # Plot the contours of the fitted Gaussian on top of the image
+        # Plot the interpolated 2D image
+        import matplotlib.pyplot as plt
+        plt.ion()
+
+        plt.figure("Interpolated Flux",clear=True)
+        plt.imshow(flux_map.T, extent=(xmin, xmax, ymin, ymax), origin="lower", aspect='auto')
+        plt.colorbar(label="Flux")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title("(Xmod,Ymod) maximum position: (%.3f,%.3f)"%(x_fit,y_fit))
+        plt.contour(grid_x, grid_y, fitted_gaussian, levels=10, colors='red', linewidths=0.8)
