5b6fcc30411f5c7a1c0070368482af856075ddbb
sylacour <sylvestre.lacour@gmail.com>
2026-07-16 09:26:07 +0200

    bug

--- plcontrol/plscripts/modulation.py
+++ plcontrol/plscripts/modulation.py
@@ -1,8 +1,8 @@
 #coding: utf8
 #%%
 import numpy as np
-import matplotlib
-matplotlib.use('macosx')
+# import matplotlib
+# matplotlib.use('macosx')
 from matplotlib import pyplot as plt
 plt.ion()
 
